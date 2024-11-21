8aaa8a0bd4cc4d6a6b3f6e1776859517e048e20f
keni <kennyranes2319@gmail.com>
2024-11-21 22:18:08 +0800

    register-page
    register-page

---
+++ register.php
@@ -0,0 +1,45 @@
+<html lang="en">
+
+<head>
+    <title>Register</title>
+    <link rel="stylesheet" type="text/css" href="other/header.css">
+    <link rel="stylesheet" type="text/css" href="css/register.css">
+</head>
+
+<body>
+
+    <?php include("other/header.php") ?>
+
+    <div class="registercard container">
+        <h1 class="regtxt">REGISTRATION</h1>
+        <form action="/action_page.php" class="container">
+        
+            <label for="fname" class="txtfields">NAME:</label>
+            <input type="text" id="fname" name="fname" class="fields">
+            <input type="text" id="fname" name="fname" class="fields">
+            <input type="text" id="fname" name="fname" class="fields"><br><br>
+            
+            <div class="names">SURNAME</div>
+
+            <div class="names">FIRST NAME</div>
+
+            <div class="names">MIDDLE NAME</div>
+                
+        
+            <label for="lname" class="txtfields">EMAIL:</label>
+            <input type="text" id="lname" name="lname" class="fields fields4">
+            <div class="empty"></div>
+            <label for="lname" class="txtfields2">CARD NUMBER:</label>
+            <input type="text" id="lname" name="lname" class="fields fields2">
+            
+        </form>
+        <button><p>REGISTER</p></button>
+    </div>
+
+    
+
+
+
+</body>
+
+</html>
