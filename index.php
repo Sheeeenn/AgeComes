c123ea95a33f76a56d134e7532f18391b76ff037
guiribajustin2004 <127721330+guiribajustin2004@users.noreply.github.com>
2024-11-21 20:02:39 +0800

    FRONTEND

---
+++ index.php
@@ -0,0 +1,19 @@
+<html>
+    <head>
+        <title>Age Comes With Price</title>
+        <link rel="stylesheet" type="text/css" href="css/index.css">
+    </head>
+
+    <body>
+        <header class="header">
+            <p class="p1">AGE COMES WITH PRICE</p>
+            <p class="p2"><i>A Purchase-Tracking System</i></p>
+        </header>
+
+        <div class="dgrid">
+            <button>REGISTER</button>
+        </div>
+
+    </body>
+
+<html>
