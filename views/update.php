062c43eb0958cc59f333cf682b3f1ed7cd283da9
keni <kennyranes2319@gmail.com>
2024-11-22 22:55:11 +0800

    update page done
    update page done

--- views/update.php
+++ views/update.php
@@ -1,7 +1,7 @@
 <html lang="en">
 
 <head>
-    <title>Register</title>
+    <title>Update</title>
     <link rel="stylesheet" type="text/css" href="other/header.css">
     <link rel="stylesheet" type="text/css" href="other/button.css">
     <link rel="stylesheet" type="text/css" href="css/update.css">
@@ -13,12 +13,27 @@
 
     <div class="container">
         <h1 class="text">TRANSACTION DETAILS</h1>
-        <div class="container2">
-            <label for="lname" class="details">Last Store Purchase:</label>
-            <input type="text" id="lname" name="lname" class="fields">
+        <div class="container2 grid">
+            <div class="line grid">
+                <label for="lname" class="details">Last Store Purchase:</label>
+                <input type="text" id="lname" name="lname" class="fields">
+            </div>
 
-            <label for="lname" class="details">Last Store Purchase:</label>
-            <input type="text" id="lname" name="lname" class="fields">
+            <div class="line grid">
+                <label for="lname" class="details">Used Budget:</label>
+                <input type="text" id="lname" name="lname" class="fields">
+            </div>
+
+            <div class="line grid">
+                <label for="lname" class="details">Date of Last Purchase:</label>
+                <input type="text" id="lname" name="lname" class="fields" readonly>
+            </div>
+
+            <div class="line grid">
+                <label for="lname" class="details">Budget Left:</label>
+                <input type="text" id="lname" name="lname" class="fields" readonly>
+            </div>
+
 
         </div>
 
