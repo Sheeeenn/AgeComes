9bf42b4145ab7b306fcedf42c5a60be036c0adcc
RANEASHBELL <kennyranes2319@gmail.com>
2024-11-22 13:24:39 +0800

    updates
    update page changes

--- views/update.php
+++ views/update.php
@@ -11,15 +11,24 @@
 
     <?php include("other/header.php") ?>
 
-    <div>
-        
+    <div class="container">
+        <h1 class="text">TRANSACTION DETAILS</h1>
+        <div class="container2">
+            <label for="lname" class="details">Last Store Purchase:</label>
+            <input type="text" id="lname" name="lname" class="fields">
+
+            <label for="lname" class="details">Last Store Purchase:</label>
+            <input type="text" id="lname" name="lname" class="fields">
+
+        </div>
 
 
-
-        <button><p>UPDATE</p></button>
+        <button>
+            <p>UPDATE</p>
+        </button>
     </div>
 
-    
+
 
 
 
