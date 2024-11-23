622c54eb8a1947ed2e4bbbdb5fb5cd309d6e083d
guiribajustin2004 <127721330+guiribajustin2004@users.noreply.github.com>
2024-11-23 16:14:40 +0800

    Register Page Update

--- backend/database/create_data.php
+++ backend/database/create_data.php
@@ -1,45 +1,45 @@
 <?php
 
-// $host = "localhost";
-// $username = "root";
-// $password = "";
-// $dbname = "AgeComes";
+$host = "localhost";
+$username = "root";
+$password = "";
+$dbname = "AgeComes";
 
-// $conn = new mysqli($host, $username, $password);
+$conn = new mysqli($host, $username, $password);
 
 
-// $sql_create_db = "CREATE DATABASE IF NOT EXISTS $dbname";
-// if ($conn->query($sql_create_db) === FALSE) {
-//     die("Error creating database: " . $conn->error);
-// }
+$sql_create_db = "CREATE DATABASE IF NOT EXISTS $dbname";
+if ($conn->query($sql_create_db) === FALSE) {
+    die("Error creating database: " . $conn->error);
+}
 
-// if (!$conn->select_db($dbname)) {
-//     die("Error selecting database: " . $conn->error);
-// }
+if (!$conn->select_db($dbname)) {
+    die("Error selecting database: " . $conn->error);
+}
 
-// if ($conn->connect_error) {
-//     die("Connection failed: " . $conn->connect_error);
-// }
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
 
-// $sql = "
-// CREATE TABLE IF NOT EXISTS AgeComes (
-//     Id INT AUTO_INCREMENT PRIMARY KEY,
-//     BarCodeNumber CHAR(9) UNIQUE NOT NULL,
-//     Email VARCHAR(255) NOT NULL,
-//     FirstName VARCHAR(255) NOT NULL,
-//     MiddleName VARCHAR(255),
-//     LastName VARCHAR(255) NOT NULL,
-//     CurrentBudget DECIMAL(7,2) NOT NULL,
-//     LastSpent DECIMAL(7,2) NOT NULL,
-//     LastDateSpent DATETIME NOT NULL,
-//     LastStoreSpent VARCHAR(255)
-// )";
+$sql = "
+CREATE TABLE IF NOT EXISTS AgeComes (
+    Id INT AUTO_INCREMENT PRIMARY KEY,
+    BarCodeNumber CHAR(9) UNIQUE NOT NULL,
+    Email VARCHAR(255) NOT NULL,
+    FirstName VARCHAR(255) NOT NULL,
+    MiddleName VARCHAR(255),
+    LastName VARCHAR(255) NOT NULL,
+    CurrentBudget DECIMAL(7,2) NOT NULL,
+    LastSpent DECIMAL(7,2) NOT NULL,
+    LastDateSpent DATETIME NOT NULL,
+    LastStoreSpent VARCHAR(255)
+)";
 
-// if ($conn->query($sql) === FALSE) {
-//     echo "Error: " . $conn->error;
-// }
+if ($conn->query($sql) === FALSE) {
+    echo "Error: " . $conn->error;
+}
 
-// $conn->close();
+$conn->close();
 
 
 ?>
