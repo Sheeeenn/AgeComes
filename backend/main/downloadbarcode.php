5ef3bf8e3d5469eb96076a52b3aac83eb52dc5d4
Zyldjan Lanticse <115306019+Zyd8@users.noreply.github.com>
2024-11-24 15:49:26 +0800

    [Add] Download barcode for client side

--- backend/main/downloadbarcode.php
+++ backend/main/downloadbarcode.php
@@ -1,13 +1,23 @@
 <?php
-
     require 'vendor/autoload.php';
     require_once("backend/start.php");
+
     $colorGreen = [0, 255, 0];
     $download_barcode = (new Picqer\Barcode\Types\TypeCodabar())->getBarcode($_SESSION['CardID']);
     $renderer = new Picqer\Barcode\Renderers\JpgRenderer();
     $renderer->setForegroundColor($colorGreen);
-   
-    file_put_contents('ouput_barcode.jpeg', $renderer->render($download_barcode, $download_barcode->getWidth() * 3, 50));
-    header("Location: /dashboard");
+
+    $filePath = 'output_barcode.jpeg';
+
+    file_put_contents($filePath, $renderer->render($download_barcode, $download_barcode->getWidth() * 3, 50));
+
+    header('Content-Type: image/jpeg');
+    header('Content-Disposition: attachment; filename="barcode.jpeg"');
+    header('Content-Length: ' . filesize($filePath));
+
+    readfile($filePath);
+
+    unlink($filePath);
+
     exit;
 ?>
